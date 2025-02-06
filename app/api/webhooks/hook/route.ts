072997da0c627434c22e189326f624c52d2f4528
LucianoDev <imakerofthings@gmail.com>
2025-02-05 22:28:37 -0300

    fix: stripe checkout active2

--- app/api/webhooks/hook/route.ts
+++ app/api/webhooks/hook/route.ts
@@ -2,41 +2,27 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-// Configuração para não processar automaticamente o corpo da requisição no Next.js
-export const config = {
-  api: {
-    bodyParser: false, // Desabilita o bodyParser do Next.js
-  },
-};
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-01-27.acacia",
+});
 
 export const POST = async (request: Request) => {
-  // Verificação das variáveis de ambiente
-  const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
   const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-  if (!stripeSecretKey || !stripeWebhookSecret) {
-    console.error("Chaves de ambiente da Stripe não configuradas.");
+  if (!stripeWebhookSecret) {
+    console.error("Chave de ambiente STRIPE_WEBHOOK_SECRET não configurada.");
     return NextResponse.error();
   }
 
-  // Extrair assinatura do cabeçalho
   const signature = request.headers.get("stripe-signature");
   if (!signature) {
     console.error("Assinatura do Stripe não encontrada.");
     return NextResponse.error();
   }
 
-  // Obter o corpo raw da requisição
-  const rawBody = await request.clone().text();
-
-  // Inicializar o cliente Stripe
-  const stripe = new Stripe(stripeSecretKey, {
-    apiVersion: "2025-01-27.acacia", // Versão da API da Stripe
-  });
+  const rawBody = await request.text();
 
   let event: Stripe.Event;
-
-  // Verificação da assinatura do webhook
   try {
     event = stripe.webhooks.constructEvent(
       rawBody,
@@ -44,37 +30,27 @@ export const POST = async (request: Request) => {
       stripeWebhookSecret,
     );
   } catch (err) {
-    console.error("Erro ao verificar assinatura do webhook:");
+    console.error("Erro ao verificar assinatura do webhook:", err);
     return NextResponse.error();
   }
 
-  // Processar os eventos recebidos
   switch (event.type) {
     case "invoice.paid":
-      console.log("Evento: invoice.paid");
       await handleInvoicePaid(event);
       break;
-
     case "customer.subscription.deleted":
-      console.log("Evento: customer.subscription.deleted");
-      await handleSubscriptionDeleted(event, stripe);
+      await handleSubscriptionDeleted(event);
       break;
-
     case "customer.subscription.updated":
-      console.log("Evento: customer.subscription.updated");
-      await handleSubscriptionUpdated(event, stripe);
+      await handleSubscriptionUpdated(event);
       break;
-
     default:
       console.log(`Evento não tratado: ${event.type}`);
-      break;
   }
 
-  // Retorno bem-sucedido
   return NextResponse.json({ received: true });
 };
 
-// Função para lidar com o evento "invoice.paid"
 const handleInvoicePaid = async (event: Stripe.Event) => {
   const invoice = event.data.object as Stripe.Invoice;
   const clerkUserId = invoice.subscription_details?.metadata?.clerk_user_id;
@@ -87,23 +63,19 @@ const handleInvoicePaid = async (event: Stripe.Event) => {
   try {
     await clerkClient.users.updateUser(clerkUserId, {
       privateMetadata: {
-        stripeCustomerId: invoice.customer as string,
-        stripeSubscriptionId: invoice.subscription as string,
+        stripeCustomerId: invoice.customer,
+        stripeSubscriptionId: invoice.subscription,
       },
       publicMetadata: {
         subscriptionPlan: "premium",
       },
     });
   } catch (err) {
-    console.error("Erro ao atualizar usuário no Clerk:");
+    console.error("Erro ao atualizar usuário no Clerk:", err);
   }
 };
 
-// Função para lidar com o evento "customer.subscription.deleted"
-const handleSubscriptionDeleted = async (
-  event: Stripe.Event,
-  stripe: Stripe,
-) => {
+const handleSubscriptionDeleted = async (event: Stripe.Event) => {
   const subscription = event.data.object as Stripe.Subscription;
   const clerkUserId = subscription.metadata.clerk_user_id;
 
@@ -123,20 +95,16 @@ const handleSubscriptionDeleted = async (
       },
     });
   } catch (err) {
-    console.error("Erro ao atualizar usuário no Clerk:");
+    console.error("Erro ao atualizar usuário no Clerk:", err);
   }
 };
 
-// Função para lidar com o evento "customer.subscription.updated"
-const handleSubscriptionUpdated = async (
-  event: Stripe.Event,
-  stripe: Stripe,
-) => {
+const handleSubscriptionUpdated = async (event: Stripe.Event) => {
   const subscription = event.data.object as Stripe.Subscription;
 
   if (subscription.cancellation_details?.reason === "cancellation_requested") {
     const clerkUserId = subscription.metadata.clerk_user_id;
-    const expirationDate = new Date(subscription.current_period_end * 1000); // Convertendo timestamp para Date
+    const expirationDate = new Date(subscription.current_period_end * 1000);
 
     console.log("Cancelamento solicitado. Data de expiração:", expirationDate);
 
@@ -156,7 +124,7 @@ const handleSubscriptionUpdated = async (
         },
       });
     } catch (err) {
-      console.error("Erro ao atualizar usuário no Clerk:");
+      console.error("Erro ao atualizar usuário no Clerk:", err);
     }
   }
 };
