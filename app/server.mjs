67dd923d1cc62a877cd19386437e48e57814b1a8
LucianoDev <imakerofthings@gmail.com>
2025-02-06 18:09:55 -0300

    feat: verificarAssinaturasExpiradas

--- app/server.mjs
+++ app/server.mjs
@@ -8,7 +8,7 @@ async function verificarAssinaturasExpiradas() {
 
     for (const user of users) {
       // Garantindo que expirationRaw seja uma string válida ou defina a data padrão
-      let expirationRaw: unknown = user.publicMetadata?.subscriptionExpiration;
+      let expirationRaw = user.publicMetadata?.subscriptionExpiration;
       console.log(expirationRaw);
 
       // Se expirationRaw não for uma string válida, defina a data padrão
@@ -17,7 +17,15 @@ async function verificarAssinaturasExpiradas() {
       }
 
       // Convertendo expirationRaw para um objeto Date
-      const expirationDate = new Date(expirationRaw as string);
+      const expirationDate = new Date(expirationRaw);
+
+      // Verificando se a data de expiração é válida
+      if (isNaN(expirationDate.getTime())) {
+        console.error(
+          `Data de expiração inválida para o usuário ${user.id}: ${expirationRaw}`,
+        );
+        continue; // Pula para o próximo usuário
+      }
 
       // Adicionando 1 dia à data de expiração
       const expirationPlusOne = new Date(expirationDate);
@@ -41,6 +49,6 @@ async function verificarAssinaturasExpiradas() {
 }
 
 // Agendar a execução todos os dias à meia-noite
-cron.schedule("0 0 * * *", verificarAssinaturasExpiradas); // Rodando à meia-noite todos os dias
+cron.schedule("15 18 * * *", verificarAssinaturasExpiradas); // Rodando à meia-noite todos os dias
 
 console.log("✅ Agendamento de verificação de assinaturas iniciado.");
