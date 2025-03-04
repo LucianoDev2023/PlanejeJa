5add3f079c7940b9bb849c763a53722545c1245c
LucianoDev <imakerofthings@gmail.com>
2025-03-04 19:57:31 -0300

    feat: google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Toaster } from "sonner";
+import Script from "next/script"; // Importando o componente Script
 
 const mulish = Mulish({
   subsets: ["latin-ext"],
@@ -42,6 +43,23 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt_BR">
+      <head>
+        {/* Usando o componente Script para carregar o Google Tag Manager */}
+        <Script
+          strategy="afterInteractive" // Isso garante que o script seja carregado após a página ser interativa
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16908282311"
+          async
+        />
+        <Script id="gtag-setup" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16908282311');
+          `}
+        </Script>
+        {/* Fim do Google Tag Manager */}
+      </head>
       <body className={`${mulish.className} dark bg-[#0D141A] antialiased`}>
         <ClerkProvider
           appearance={{
