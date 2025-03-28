de0741cde3909f49a1409fae0240de0e0442d72a
LucianoDev <imakerofthings@gmail.com>
2025-03-28 19:10:54 -0300

    feat(api): removendo o tour

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -165,7 +165,7 @@ const Home = async ({ searchParams: { month, year, opcao } }: HomeProps) => {
           </div>
         </div>
       </div>
-      <Tour />
+      {/* <Tour /> */}
     </div>
   );
 };
