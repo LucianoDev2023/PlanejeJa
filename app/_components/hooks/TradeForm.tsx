cb7dc2394c4d6848f10035d9456a9a4d60dfe2cf
LucianoDev <imakerofthings@gmail.com>
2025-03-28 23:55:32 -0300

    feat(api): add tour button com storage

--- app/_components/hooks/TradeForm.tsx
+++ app/_components/hooks/TradeForm.tsx
@@ -171,8 +171,8 @@ export default function TradeForm({
 
   return (
     <div className="w-full border-b border-gray-800 bg-[#060D13] p-4 shadow-md">
-      <p className="mt-2 pb-2 text-center font-sans text-sm font-normal text-gray-300 sm:m-0 sm:pl-2 sm:text-base">
-        Selecione o registro de compra ou venda
+      <p className="pb-3 text-center font-sans text-sm font-normal text-gray-300 sm:m-0 sm:mb-2 sm:mt-2 sm:pl-2 sm:text-base">
+        Faça o registro de compra ou venda
       </p>
       <form
         onSubmit={handleSubmit}
